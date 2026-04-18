95cf2e2def5e4e7341d6c26e9328a950131afe52
Pedro <pedrocastro014@gmail.com>
2026-04-18 17:55:04 -0300

    refiz o data.table

--- modules/mod_QC4DIANN.r
+++ modules/mod_QC4DIANN.r
@@ -446,10 +446,10 @@ QC4DIANN_server <- function(id) {
         Lib.PG.Q.Value <= 0.01 &
         Lib.Q.Value <= 0.01 &
         PG.Q.Value <= 0.01 &
-        # If (0 <= input <= 1), use the input, else use 0
-        PG.MaxLFQ.Quality >= ifelse(input$PG.MaxLFQ.Quality %between% c(0,1), input$PG.MaxLFQ.Quality, 0) &
-        # If (0 <= input <= 1), use the input, else use 0
-        Empirical.Quality >= ifelse(input$Empirical.Quality %between% c(0,1), input$Empirical.Quality, 0)
+        # If input is null, use zero else use the user input
+        PG.MaxLFQ.Quality >= ifelse(is.null(input$PG.MaxLFQ.Quality), 0, input$PG.MaxLFQ.Quality) &
+        # If input is null, use zero else use the user input
+        Empirical.Quality >= ifelse(is.null(input$Empirical.Quality), 0, input$Empirical.Quality)
       ]
       
       # TODO: input$cRAP was not implemented yet #######
@@ -520,14 +520,24 @@ QC4DIANN_server <- function(id) {
 
     QuantUMS_scores <- reactive({
       req(input$report)
-      arrow::read_parquet(input$report$datapath) %>%
-        dplyr::filter(
-          Lib.PG.Q.Value <= 0.01,
-          Lib.Q.Value <= 0.01,
-          PG.Q.Value <= 0.01,
-          !stringr::str_detect(Protein.Ids, "cRAP")
-        ) %>%
-        dplyr::select(
+      data_parquet <- arrow::read_parquet(input$report$datapath) %>%
+        data.table::as.data.table() # Dataset must be a data.table from data.table package
+      
+      # Filters
+      data_parquet <- data_parquet[
+        Lib.PG.Q.Value <= 0.01 &
+        Lib.Q.Value <= 0.01 &
+        PG.Q.Value <= 0.01 
+      ]
+      
+      # TODO: input$cRAP was not implemented yet #######
+      # # If the user select to remove the cRAP, remove it
+      # if(input$cRAP) {
+      #   data_parquet <- data_parquet[!grepl("cRAP", Protein.Ids)]
+      # }
+      
+      # Select columns
+      data_parquet[, .(
           Run,
           Precursor.Id,
           PG.MaxLFQ.Quality,
@@ -581,13 +591,18 @@ QC4DIANN_server <- function(id) {
 
     MS_corr <- reactive({
       req(input$report)
-      arrow::read_parquet(input$report$datapath) %>%
-        dplyr::filter(
-          Lib.PG.Q.Value <= 0.01,
-          Lib.Q.Value <= 0.01,
-          PG.Q.Value <= 0.01
-        ) %>%
-        dplyr::mutate(File.Name = Run)
+      data_parquet <- arrow::read_parquet(input$report$datapath) %>%
+        data.table::as.data.table() # Dataset must be a data.table from data.table package
+      
+      # Filters
+      data_parquet <- data_parquet[
+        Lib.PG.Q.Value <= 0.01 &
+        Lib.Q.Value <= 0.01 &
+        PG.Q.Value <= 0.01 
+      ]    
+    
+      # Mutate
+      data_parquet <- data_parquet[, File.Name := Run]
     })
 
     PCA_label <- reactive({
